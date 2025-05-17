cae3b8f720f50ef056c29c70098b7f566395ee03
igor-alexandre-bp <barbosa334@gmail.com>
2025-05-17 11:13:39 -0300

    Mudança de indentação em códigos já feitos e criação de aparência base na página de produtos.

--- src/app/component/pagina-inicial/pagina-inicial.component.ts
+++ src/app/component/pagina-inicial/pagina-inicial.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NgFor } from '@angular/common';
-import { CommonModule } from '@angular/common';
+import { NgFor, CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { ProdutoComponent } from '../estrutura/produto/produto.component';
 
@@ -9,46 +8,47 @@ import { ProdutoComponent } from '../estrutura/produto/produto.component';
   standalone: true,
   imports: [NgFor, CommonModule, ProdutoComponent],
   templateUrl: './pagina-inicial.component.html',
-  styleUrls: ['./pagina-inicial.component.css']
+  styleUrl: './pagina-inicial.component.css'
 })
+
 export class PaginaInicialComponent implements OnInit {
- produtos = [
-  {
-    id: 1,
-    imagem: '../../../assets/produtos/Samsung_Galax_S25_base_azul.jpg',
-    descricao: 'Samsung Galaxy S25 - Tela infinita e alto desempenho',
-    preco: 3999.90
-  },
-  {
-    id: 2,
-    imagem: '../../../assets/produtos/Smart_TV_4K_LG_UHD_70.jpg',
-    descricao: 'Smart TV 4K LG 70" com Inteligência Artificial',
-    preco: 4599.00
-  },
-  {
-    id: 3,
-    imagem: '../../../assets/produtos/Geladeira_Electrolux_Frost_Free_490L.jpg',
-    descricao: 'Geladeira Electrolux 490L Frost Free com Inox',
-    preco: 3199.99
-  },
-  {
-    id: 4,
-    imagem: '../../../assets/produtos/WAP_Parafusadeira_e_Furadeira_a_Bateria_com_Kit_de_13_Acessorios_e_Maleta_Empunhadura_Emborrachada_Bivolt.jpg',
-    descricao: 'Furadeira WAP com 13 acessórios e maleta',
-    preco: 499.90
-  },
-  {
-    id: 5,
-    imagem: '../../../assets/produtos/MAGE_MALE_Jaqueta_masculina.jpg',
-    descricao: 'Jaqueta masculina MAGE MALE - Estilo urbano',
-    preco: 249.90
-  },
-  {
-    id: 6,
-    imagem: '../../../assets/produtos/Mochila_Bolsa_Notebook_Reforcada_Anti_Furto_Semi_Impermeavel.jpg',
-    descricao: 'Mochila anti-furto reforçada e semi impermeável',
-    preco: 179.00
-  }
+  produtos = [
+    {
+      id: 1,
+      imagem: '../../../assets/produtos/Samsung_Galax_S25_base_azul.jpg',
+      descricao: 'Samsung Galaxy S25 - Tela infinita e alto desempenho',
+      preco: 3999.90
+    },
+    {
+      id: 2,
+      imagem: '../../../assets/produtos/Smart_TV_4K_LG_UHD_70.jpg',
+      descricao: 'Smart TV 4K LG 70" com Inteligência Artificial',
+      preco: 4599.00
+    },
+    {
+      id: 3,
+      imagem: '../../../assets/produtos/Geladeira_Electrolux_Frost_Free_490L.jpg',
+      descricao: 'Geladeira Electrolux 490L Frost Free com Inox',
+      preco: 3199.99
+    },
+    {
+      id: 4,
+      imagem: '../../../assets/produtos/WAP_Parafusadeira_e_Furadeira_a_Bateria_com_Kit_de_13_Acessorios_e_Maleta_Empunhadura_Emborrachada_Bivolt.jpg',
+      descricao: 'Furadeira WAP com 13 acessórios e maleta',
+      preco: 499.90
+    },
+    {
+      id: 5,
+      imagem: '../../../assets/produtos/MAGE_MALE_Jaqueta_masculina.jpg',
+      descricao: 'Jaqueta masculina MAGE MALE - Estilo urbano',
+      preco: 249.90
+    },
+    {
+      id: 6,
+      imagem: '../../../assets/produtos/Mochila_Bolsa_Notebook_Reforcada_Anti_Furto_Semi_Impermeavel.jpg',
+      descricao: 'Mochila anti-furto reforçada e semi impermeável',
+      preco: 179.00
+    }
   ];
 
   visiveis: any[] = [];
@@ -61,7 +61,9 @@ export class PaginaInicialComponent implements OnInit {
     this.atualizarVisiveis();
     this.iniciarCarrossel();
   }
+
   offset = 0;
+  
   atualizarVisiveis() {
     const totalCards = this.produtos.length;
     const visibleCount = 3;
@@ -70,6 +72,7 @@ export class PaginaInicialComponent implements OnInit {
     }
     this.offset = this.startIndex * 300; // 275 + gap estimado
   }
+
   iniciarCarrossel() {
     this.intervaloId = setInterval(() => {
       this.startIndex = (this.startIndex + 1) % this.produtos.length;
